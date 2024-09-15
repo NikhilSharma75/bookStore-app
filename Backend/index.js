01c21e86282088342cf54874a1a8075596d08606
Nikhil85 <nikhilsharma29102002@gmail.com>
2024-09-16 00:07:10 +0530

    app completed

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from "cors";
 import bookRoute from "../Backend/route/book.route.js"
 import userRoute from "../Backend/route/user.route.js"
+import path from "path";
 const app = express()
 app.use(cors());
 app.use(express.json())
@@ -26,6 +27,18 @@ try {
 // defining route
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+ 
+// deployment
+if(
+    process.env.NODE_ENV === "production"){
+const dirPath = path.resolve();
+app.use(express.static("Frontend/dist"));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(dirPath,"Frontend","dist","index.html"));
+})
+    }
+
+
 
 app.listen(PORT, () => {
   console.log(` Server is listening on port ${PORT}`)
